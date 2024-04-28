16ea877a43972eafbcfa828348317e13b23592ae
kendell <kenren@udel.edu>
2024-04-27 20:19:59 -0400

    CategoryRoutes are added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,16 +27,66 @@ router.get('/:id', async (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
+router.post('/:name', async (req, res) => {
+  const NewCategoryName=req.params.name
   // create a new category
+  try{
+    const NewCategory = await Category.create({category_name:NewCategoryName})
+    res.send(NewCategory)
+  } catch(err){
+    log(err)
+    res.send(err)
+  }  
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const UpdateID=req.params.id
+  const UpdateBody=req.body
+  try{
+    // get a  instance of a category
+    const CurrentCategory = await Category.findByPk(UpdateID)
+    if(!CurrentCategory){
+      return res.send({message:"Id is not found"})
+    }
+    // update it 
+      CurrentCategory.category_name = UpdateBody.Newname
+    // save it
+      await CurrentCategory.save()
+    res.send({message:`ID:${UpdateID} is updated with name ${UpdateBody}`})
+  } catch(err){
+    log(err)
+    res.send(err)
+  }  
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+  
+    const RemoveID = req.params.id
+  
+    const CurrentCategory = await Category.findByPk(RemoveID)
+  
+    if (!CurrentCategory) {
+  
+      return res.send({ message: "ID is not found" })
+    
+    }
+  
+    await CurrentCategory.destroy()
+    res.send({message:`ID:${RemoveID} is removed`})
+  } catch (err) {
+    
+    log(err)
+
+    res.send(err)
+  }
+
+
+ 
+  
+
 });
 
 module.exports = router;
